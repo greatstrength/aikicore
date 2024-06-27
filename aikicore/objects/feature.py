58938c998d95d4868d9bcc101bf8de87f176d69c
Ashatz <andrew@greatstrength.me>
2024-06-26 17:32:12 -0700

    Objects : Update - Feature : Feature Handler // Remove Use Services Attribute

--- aikicore/objects/feature.py
+++ aikicore/objects/feature.py
@@ -6,7 +6,6 @@ class FeatureHandler(Model):
     name = t.StringType(required=True)
     function_path = t.StringType(required=True)
     data_mapping = t.StringType()
-    use_services = t.StringType()
     params = t.DictType(t.StringType(), default={})
     log_activity = t.BooleanType(default=True)
 
